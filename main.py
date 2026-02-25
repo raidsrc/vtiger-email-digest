0d6e360577a993748f3fbc2e1de774dc064d49ac
Ray Legion <ray.chen@virovek.com>
2026-02-24 17:33:32 -0800

    farewell sqlite3. it's mongo time. got sample documents in mongodb and communication working.

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from fastapi import FastAPI
 from datetime import datetime
-import sqlite3
 from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
-import json
 from fastapi.responses import HTMLResponse
+from pymongo import MongoClient
+from pymongo.database import Database
+from typing import TypedDict
 
 load_dotenv()
 app = FastAPI()
-connection = sqlite3.connect("db.db")
-cursor = connection.cursor()
-
-EMAIL_SETTINGS_RECIPIENTS = os.getenv("EMAIL_SETTINGS_RECIPIENTS")
-EMAIL_SETTINGS_CC = os.getenv("EMAIL_SETTINGS_CC")
-EMAIL_SETTINGS_BCC = os.getenv("EMAIL_SETTINGS_BCC")
 
 
-class Project(BaseModel):
+class ProjectRequestBody(BaseModel):
     contactid: str
     projectstatus: str
     cf_project_activities: str
@@ -45,26 +40,52 @@ class Project(BaseModel):
     url: str
 
 
+class Project(TypedDict):
+    contactid: str
+    projectstatus: str
+    cf_project_activities: str
+    projectname: str
+    linktoaccountscontacts: str
+    cf_project_clonename: str
+    cf_project_lotnumber: str
+    project_no: str
+    cf_project_laststatuschange: str
+    cf_project_relatedorganization: str
+    cf_project_usecustomerbuffer: str
+    cf_project_projectnotesfromquote: str
+    cf_project_quotenumber: str
+    cf_project_goisize: str
+    description: str
+    cf_project_aavname: str
+    cf_project_aavserotype: str
+    cf_project_productionscale: str
+    cf_project_concentrationrequirement: str
+    cf_project_buffer: str
+    cf_project_deliveryvolume: str
+    createdtime: str
+    modifiedtime: str
+    id: str
+    url: str
+
+
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+uri = f"mongodb+srv://admin:{MONGO_PASSWORD}@cluster0.ps7aafk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client: MongoClient = MongoClient(uri)
+db: Database[Project] = client["vtigerEmailDigestDatabase"]
+QUEUE_COLLECTION = os.getenv("QUEUE_COLLECTION") or ""
+TRASH_COLLECTION = os.getenv("TRASH_COLLECTION") or ""
+db_queue_collection = db[QUEUE_COLLECTION]
+db_trash_collection = db[TRASH_COLLECTION]
+
+EMAIL_SETTINGS_RECIPIENTS = os.getenv("EMAIL_SETTINGS_RECIPIENTS")
+EMAIL_SETTINGS_CC = os.getenv("EMAIL_SETTINGS_CC")
+EMAIL_SETTINGS_BCC = os.getenv("EMAIL_SETTINGS_BCC")
+
+
 @app.get("/")
 def read_root():
     now_str = str(datetime.now())
     return {"name": "vtiger-email-digest", "date": now_str, "hello": "world"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.get("/test-endpoint/{number}")
-def read_item_2(number: int, q: str | None = None):
-    number_squared = number**2
-    response_q: str = ""
-    if q is None:
-        response_q = "q is empty."
-    else:
-        response_q = q
-    return {"your_number_squared": number_squared, "response_q": response_q}
 
 
 # view projects currently in queue (not sent yet)
@@ -77,7 +98,7 @@ def view_queue():
 
 # add a project to the projects queue.
 @app.post("/actions/projects/queue")
-async def add_project_to_queue(project: Project):
+async def add_project_to_queue(project: ProjectRequestBody):
     # when post req received, break down the body into all the pieces and assign to vars
     # write to db like so:
     """
@@ -128,11 +149,16 @@ def trigger_email():
     # loop through the list
     # for every project, construct an html string
 
-    projects = None
-    # for now, just practicing getting it working. i'm using a local file to test.
-    with open("./many-projects-sample.json") as file:
-        content = file.read()
-        projects = json.loads(content)
+    projects: list[Project] = []
+
+    # # for now, just practicing getting it working. i'm using a local file to test.
+    # with open("./many-projects-sample.json") as file:
+    #     content = file.read()
+    #     projects = json.loads(content)
+
+    projectsCursor = db_queue_collection.find()
+    for project in projectsCursor:
+        projects.append(project)
     projects = sorted(projects, key=lambda project: project["cf_project_activities"])
 
     rows_string = ""
