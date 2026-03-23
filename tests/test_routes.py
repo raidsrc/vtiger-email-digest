b516b18c8414faa61b523682aecbc61940fe39d8
Ray Legion <ray.chen@virovek.com>
2026-03-23 12:46:47 -0700

    not testing routes. not worth it

---
+++ tests/test_routes.py
@@ -0,0 +1,5 @@
+"""
+imma keep it 100, this app is so small. i'm not gonna bother writing tests for the routes. i'm very sure that they work. plus then i'd have to do testing with the database and either mock the db calls or set up a temporary docker container with mongodb running for testing and that would mean i'd have to rewrite my routes to put another layer of abstraction between my app and the db calls and i'm NOT DOING THAT!
+"""
+
+pass
