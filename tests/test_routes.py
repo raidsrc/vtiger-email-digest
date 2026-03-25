16857fa72bedded6d3fb54568a0d8f10326cc633
Ray Legion <ray.chen@virovek.com>
2026-03-25 11:04:26 -0700

    well i've gotten it working, i think i can merge to dev. lemme run this first rq

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,6 +18,7 @@ from app.class_types import ProjectRequestBody
     "input_project, results_to_check",
     [
         (
+            # not behind schedule
             {
                 "projectstatus": "Started",
                 "cf_project_activities": "DNA",
@@ -39,6 +40,7 @@ from app.class_types import ProjectRequestBody
             },
         ),
         (
+            # behind schedule no upsert
             {
                 "projectstatus": "Cloning completed",
                 "cf_project_activities": "Cloning",
@@ -54,12 +56,13 @@ from app.class_types import ProjectRequestBody
             },
             {
                 "upserted": False,
-                "behind_schedule": False,
+                "behind_schedule": True,
                 "project_name": "Started Project 456",
                 "modified_time": "2000-01-01 13:00:00",
             },
         ),
         (
+            # behind schedule yes upsert
             {
                 "projectstatus": "Cloning completed",
                 "cf_project_activities": "Cloning",
@@ -77,7 +80,7 @@ from app.class_types import ProjectRequestBody
                 "upserted": True,
                 "behind_schedule": True,
                 "project_name": "Upserted project",
-                "modified_time": "2001-01-01 13:00:00",
+                "modified_time": "2001-01-01 07:00:00",
             },
         ),
     ],
@@ -99,83 +102,6 @@ def test_add_project_to_queue(input_project, results_to_check):
         add_response["document_added_to_database"]["project"]["modifiedtime"]
         == results_to_check["modified_time"]
     )
-
-
-def test_add_project_to_queue_1():
-    # add not behind schedule not upsert
-    args = {
-        "projectstatus": "Started",
-        "cf_project_activities": "DNA",
-        "projectname": "Started Project 123",
-        "cf_project_clonename": "",
-        "cf_project_lotnumber": "",
-        "project_no": "PROJ983274298",
-        "cf_project_quotenumber": "",
-        "description": "",
-        "cf_project_aavname": "",
-        "behind_schedule": "",
-        "modifiedtime": "",
-    }
-    p = ProjectRequestBody(**args)
-    add_response = add_project_to_queue(p)
-    assert add_response.get("success") == True
-    assert add_response.get("upserted") == False
-    add_response_doc = add_response.get("document_added_to_database")
-    assert add_response_doc is not None
-    assert add_response_doc["project"]["projectname"] == "Started Project 123"
-
-
-def test_add_project_to_queue_2():
-    # add behind schedule no upsert
-    args = {
-        "projectstatus": "Cloning completed",
-        "cf_project_activities": "Cloning",
-        "projectname": "Started Project 456",
-        "cf_project_clonename": "RAY01",
-        "cf_project_lotnumber": "",
-        "project_no": "PROJ00000301",
-        "cf_project_quotenumber": "",
-        "description": "",
-        "cf_project_aavname": "",
-        "behind_schedule": True,
-        "modifiedtime": "2000-01-01 13:00:00",
-    }
-    p = ProjectRequestBody(**args)
-    add_response = add_project_to_queue(p)
-    assert add_response.get("success") == True
-    assert add_response.get("upserted") == False
-    add_response_doc = add_response.get("document_added_to_database")
-    assert add_response_doc is not None
-    assert add_response_doc["project"]["projectname"] == "Started Project 456"
-    m: str = add_response_doc["project"]["modifiedtime"]
-    assert "2000" in m
-
-
-def test_add_project_to_queue_3():
-    # add behind schedule yes upsert
-    args = {
-        "projectstatus": "Cloning completed",
-        "cf_project_activities": "Cloning",
-        "projectname": "Upserted project",
-        "cf_project_clonename": "RAY01",
-        "cf_project_lotnumber": "",
-        "project_no": "PROJ00000301",
-        "cf_project_quotenumber": "",
-        "description": "",
-        "cf_project_aavname": "",
-        "behind_schedule": True,
-        "modifiedtime": "2001-01-01 13:00:00",
-    }
-    p = ProjectRequestBody(**args)
-    add_response = add_project_to_queue(p)
-    assert add_response.get("success") == True
-    assert add_response.get("upserted") == True
-    add_response_doc = add_response.get("document_added_to_database")
-    assert add_response_doc is not None
-    assert add_response_doc["project"]["projectname"] == "Upserted project"
-    assert add_response_doc["project"]["project_no"] == "PROJ00000301"
-    m: str = add_response_doc["project"]["modifiedtime"]
-    assert "2001" in m
 
 
 def test_clear_queue():
